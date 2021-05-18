19d929ff0e1bb366f6e9563c3995b43a79650451
Pavel Popeskul <peterjj91@gmail.com>
2021-05-18 06:23:09 +0300

    lesson 1: area_of_triangle - move .to_f to num with dot

--- lesson_1/area_of_triangle.rb
+++ lesson_1/area_of_triangle.rb
@@ -4,6 +4,6 @@ basis = gets.chomp.to_i
 puts 'What height?'
 height = gets.chomp.to_i
 
-area_triangle =  1.to_f / 2 * basis * height
+area_triangle =  1.0 / 2 * basis * height
 
 puts "Result: #{area_triangle}"
