ba2d2cf021406ffe7d06a258d03281db2591f28a
Pavel Popeskul <peterjj91@gmail.com>
2021-05-19 11:18:13 +0300

    lesson 2: refactored vowels

--- lesson_2/vowels.rb
+++ lesson_2/vowels.rb
@@ -1,4 +1,3 @@
 vowels = [ "a", "e", "i", "o", "u" ]
-symbol_az = Hash[(:a..:z).to_a.zip((1..26).to_a)].select do |letter, number| 
-  vowels.include?(letter.to_s)
-end
+alphabet = Hash[(:a..:z).to_a.zip((1..26).to_a)]
+include_vowels = alphabet.select { |letter, number| vowels.include?(letter.to_s) }
