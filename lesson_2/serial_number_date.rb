7d3dd5c699daf7ec6abf2102d00a58fea44e1750
Pavel Popeskul <peterjj91@gmail.com>
2021-05-19 11:18:52 +0300

    lesson 2: correct name for february

--- lesson_2/serial_number_date.rb
+++ lesson_2/serial_number_date.rb
@@ -8,9 +8,9 @@ puts "Введите год"
 year_user = gets.chomp.to_i
 
 is_leap_year = year_user % 400 == 0 || year_user % 4 == 0 && year_user % 100 != 0;
-febraury = is_leap_year ? 29 : 28
+february = is_leap_year ? 29 : 28
 
-months = [31, febraury, 31, 31, 30, 31, 30, 31, 30, 31, 30, 31]
+months = [31, february, 31, 31, 30, 31, 30, 31, 30, 31, 30, 31]
 
 day_user = months.take(month_user).sum
 
