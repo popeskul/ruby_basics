d703ea1612e6abfd9f4472b836a758a2143d2c3f
Pavlo Popeskul <ppopeskul@lohika.com>
2021-05-26 10:12:03 +0300

    lesson 10: created the Acсessors attr_accessor_with_history/strong_attr_accessor

--- lesson_10/cargo_train.rb
+++ lesson_10/cargo_train.rb
@@ -1,6 +1,12 @@
 require_relative 'train'
+require_relative 'accessors'
 
 class CargoTrain < Train
+  include Accessors
+
+  attr_accessor_with_history :first, :last
+  strong_attr_accessor :strong, String
+
   def initialize(train_num)
     super(train_num, Train::TYPE[:CARGO])
   end
