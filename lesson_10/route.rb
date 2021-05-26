0a286a5363efe8ab01a9677b609149363fe8c423
Pavel Popeskul <peterjj91@gmail.com>
2021-05-26 08:26:50 +0300

    lesson 10: copy from previous lesson

---
+++ lesson_10/route.rb
@@ -0,0 +1,31 @@
+require_relative 'instance_counter'
+require_relative 'validation'
+
+class Route
+  include InstanceCounter
+
+  attr_reader :name, :stations
+
+  NAME_FORMAT = /^\S/
+
+  def initialize(name, station_start, station_end)
+    @name = name
+    @stations = [station_start, station_end]
+    validate?
+    register_instance
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def delete_station(station)
+    @stations.delete(station)
+  end
+
+  private
+
+  def validate?
+    raise ValidationError, 'Ошибка! Имя должно состоять из одного символа и без пробелов' if @name !~ NAME_FORMAT
+  end
+end
